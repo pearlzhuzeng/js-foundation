f923afc07c4f992f6be1b6314ab6997a29d43560
Pearl Zeng <zhuzeng@umich.edu>
2017-09-26 14:29:13 -0400

    [WIP] silly story generator
    https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Silly_story_generator

---
+++ scripts/silly-story-generator.js
@@ -0,0 +1,48 @@
+// COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// RAW TEXT STRINGS
+const storyText =
+  'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = [
+  'spontaneously combusted',
+  'melted into a puddle on the sidewalk',
+  'turned into a slug and crawled away',
+];
+
+// EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+const newStory = storyText; // So we can create a new random story each time the button is pressed. If we made changes directly to storyText, we'd only be able to generate a new story once.
+const xItem = randomValueFromArray(insertX);
+const yItem = randomValueFromArray(insertY);
+const zItem = randomValueFromArray(insertZ);
+
+randomize.addEventListener('click', result);
+
+function result() {
+  newStory.replace(':insertx:', xItem);
+  newStory.replace(':inserty:', yItem);
+  newStory.replace(':insertz:', zItem);
+
+  if (customName.value != '') {
+    const name = customName.value;
+    newStory.replace('Bob', name);
+  }
+
+  if (document.getElementById('uk').checked) {
+    const temperature = `${Math.round((94 - 32) * 5 / 9)} centigrade`;
+    const weight = `${Math.round(300 / 14)} stone`;
+    newStory.replace('94 farenheit', temperature);
+    newStory.replace('300 pounds', weight);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
